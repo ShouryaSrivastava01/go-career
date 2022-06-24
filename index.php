fe59e5f11be98b997c8b115ce9457d0781e71067
ShouryaSrivastava01 <72685413+ShouryaSrivastava01@users.noreply.github.com>
2022-06-24 12:37:02 +0530

    Add files via upload

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1">
 
-  <title>Homepage</title>
+  <title>Go Career</title>
   <!-- Fonts -->
   <link rel="stylesheet" href="css/bootstrap.min.css">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
